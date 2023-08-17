b2d2d9f5019ea9d3234477af9342fe17bf22e270
Mauro <achipizgualimauricio@gmial.com>
2023-08-17 11:38:58 -0500

    arreglo de la ventana de notificaciones

--- view/categorias/despensa.php
+++ view/categorias/despensa.php
@@ -1,3 +1,4 @@
+
 <main class="container-lg">
     <h1 class="page-header text-center text-white" style="margin: 20px; padding: 20px; background: rgba(0, 108, 248, 0.603); border-radius: 20px; ">Despensa</h1>
     <div class="container-producto container-fluid">
