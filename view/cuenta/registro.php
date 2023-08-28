111189b80dd81281c51d80e48a2f7d966ee783a6
GUANKI <jm065726@gmail.com>
2023-08-28 11:40:51 -0500

    aviso de contraseña

--- view/cuenta/registro.php
+++ view/cuenta/registro.php
@@ -1,49 +1,49 @@
 <div class="container py-4">
-        <div>
-            <div style="justify-content: center ;" class="d-flex">
-                <img style="width: 120px; height: 100px;" src="logo proyecto.png" alt="">
-            </div>
+    <div>
+        <div style="justify-content: center ;" class="d-flex">
+            <img style="width: 120px; height: 100px;" src="logo proyecto.png" alt="">
         </div>
-        <div>
-            <form  class="row mt-2" method="post" action="?c=usuario&a=Registrar">
-                <div class="col-6">
-                    <label class="form-label" for="nom_cli">Nombres</label>
-                    <input class="form-control" type="text" name="nom_cli">
-                </div>
-                <div class="col-6 mb-3">
-                    <label class="form-label" for="ape_cli">Apellidos</label>
-                    <input class="form-control" type="text" name="ape_cli">
-                </div>
-                <div class="col-3" >
-                    <label class="form-label" for="cor_cli">Nombre de usuario</label>
-                    <input class="form-control" type="text" name="user_cli">
-                </div>
-                <div class="col-4" >
-                    <label class="form-label" for="cor_cli">Correo Electrónico</label>
-                    <input class="form-control" type="text" name="cor_cli">
-                </div>
-                <div class="col-5 mb-3">
-                    <label class="form-label" for="pas_cli">Contraseña</label>
-                    <input class="form-control" type="password" name="pas_cli">
-                </div>
-                <div class="col-4">
-                    <label class="form-label" for="fch_cli">Fecha de nacimiento</label>
-                    <input class="form-control" type="date" name="fch_cli">
-                </div>
-                <div class="col-4">
-                    <label class="form-label" for="cel_cli">Número de Celular</label>
-                    <input class="form-control" type="text" maxlength="10" name="cel_cli">
-                </div>
-                <div class="col-4 mb-3">
-                    <label class="form-label" for="dir_cli">Dirección de Domicilio</label>
-                    <input class="form-control" type="text" name="dir_cli">
-                </div>
-                <div class="d-flex justify-content-center">
-                    <input class="btn btn-success" type="submit" value="Registrarme">
-                </div>
-            </form>
-        </div>
-        <?php
-
-        ?>
     </div>
+    <div>
+        <form class="row mt-2" method="post" action="?c=usuario&a=Registrar">
+            <div class="col-6">
+                <label class="form-label" for="nom_cli">Nombres</label>
+                <input class="form-control" type="text" name="nom_cli">
+            </div>
+            <div class="col-6 mb-3">
+                <label class="form-label" for="ape_cli">Apellidos</label>
+                <input class="form-control" type="text" name="ape_cli">
+            </div>
+            <div class="col-3">
+                <label class="form-label" for="cor_cli">Nombre de usuario</label>
+                <input class="form-control" type="text" name="user_cli">
+            </div>
+            <div class="col-4">
+                <label class="form-label" for="cor_cli">Correo Electrónico</label>
+                <input class="form-control" type="text" name="cor_cli">
+            </div>
+            <div class="col-5 mb-3">
+                <label class="form-label"  for="pas_cli">Contraseña</label>
+                <input class="form-control" type="password" name="pas_cli"  data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Contraseña minimo de 8 caracteres. Debe contener minimo una mayusculas                                                                                                                    " >
+            </div>
+            <div class="col-4">
+                <label class="form-label" for="fch_cli">Fecha de nacimiento</label>
+                <input class="form-control" type="date" name="fch_cli" >
+            </div>
+            <div class="col-4">
+                <label class="form-label" for="cel_cli">Número de Celular</label>
+                <input class="form-control" type="text" maxlength="10" name="cel_cli">
+            </div>
+            <div class="col-4 mb-3">
+                <label class="form-label" for="dir_cli">Dirección de Domicilio</label>
+                <input class="form-control" type="text" name="dir_cli">
+            </div>
+            <div class="d-flex justify-content-center">
+                <input class="btn btn-success" type="submit" value="Registrarme">
+            </div>
+        </form>
+    </div>
+    <?php
+
+    ?>
+</div>
